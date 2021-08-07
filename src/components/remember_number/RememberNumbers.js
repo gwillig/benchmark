14eddf91ab7231cb6d1b4738772440afae5c2839
gwillig <gustavwillig@gmail.com>
2021-08-07 14:07:28 +0200

    FEAT| frontend|add input fields to make it easier to describe the circumstance

--- src/components/remember_number/RememberNumbers.js
+++ src/components/remember_number/RememberNumbers.js
@@ -1,4 +1,5 @@
 import React from "react"
+import {propsAsString} from "./util"
 import {Container, Row, Col, Button, Card} from "react-bootstrap"
 import { CountdownCircleTimer } from 'react-countdown-circle-timer'
 import axios from "axios";
@@ -25,6 +26,12 @@ class RememberNumbers extends React.Component{
             userInput:"",
             currentState:",",
             note:"",
+            time:null,
+            surrounding:null,
+            meal:null,
+            sleep:null,
+            music:null,
+            special:null,
             countDownTimer:2,
             timePerLevel:2,
             inputOptions:[]
@@ -35,6 +42,7 @@ class RememberNumbers extends React.Component{
         this.handleChangeTextarea = this.handleChangeTextarea.bind(this)
         this.get_distinct_notes = this.get_distinct_notes.bind(this)
         this.createCountdown = this.createCountdown.bind(this)
+        this.handleInputField = this.handleInputField.bind(this)
     }
 
     startBtn=()=>{
@@ -64,6 +72,24 @@ class RememberNumbers extends React.Component{
         return [true, 1 ]
     }
 
+    handleInputField(event,inputFieldname){
+        let {time,surrounding,meal,sleep,music,special}=this.state
+        const note={
+            time:time,
+            surrounding:surrounding,
+            meal:meal,
+            sleep:sleep,
+            music:music,
+            special:special
+        }
+        //.Step: Replace changed value
+        note[inputFieldname] = event.target.value
+        this.setState((prevState, props) => ({
+              note: propsAsString(note),
+              [inputFieldname]:event.target.value
+            }));
+
+    }
     handleChange(event) {
         this.setState({userInput: event.target.value});
 
@@ -151,7 +177,6 @@ class RememberNumbers extends React.Component{
         const {displayNewGame, isPlaying, displayGame, displayUserInput, randomNumber,currentState,userInput
               ,displayResult,level, prevLevel,inputOptions, countDownTimer } = this.state
 
-
         return(
                 <Container id="RememberNumberContainer" fluid className="align-middle">
                 <Row className="justify-content-md-center">
@@ -159,14 +184,89 @@ class RememberNumbers extends React.Component{
                       <h1 className="text-center" >Number Memory</h1>
                   </Col>
                 </Row>
-
                 <Row  id="newGame" className="" className={displayNewGame}>
                     <div className="mx-auto d-block"  >
                         <Card  className="text-center" style={{ width: '22rem' }} className="text-center" >
                           <Card.Body>
                           <Card.Title>New Game</Card.Title>
-                              <input style={{width:"100%"}} onChange={this.handleChangeTextarea}
-                                   list="notes" id="note" name="note"
+                              <div id="timeInputGroup">
+                                  <label className="inputLabels">Time &nbsp;</label>
+                                    <input className="inputField" list="timeList"
+                                           onChange={(event)=>this.handleInputField(event,"time")}
+                                    />
+                                    <datalist id="timeList">
+                                      <option value="earlyMorning" >5:00 - 08:00</option>
+                                      <option value="morning" >8:00 - 12:00</option>
+                                      <option value="earlyAfternoon" >12:00 - 15:00</option>
+                                      <option value="afternoon" >15:00 - 17:00</option>
+                                      <option value="earlyEvening" >17:00 - 19:00</option>
+                                      <option value="evening" >19:00 - 21:00</option>
+                                      <option value="lateEvening" >21:00 - 23:00</option>
+                                      <option value="Night" >23:00 - 5:00</option>
+                                    </datalist>
+                              </div>
+                              <div id="surroundingInputGroup">
+                                  <label className="inputLabels">Sur. &nbsp;</label>
+                                    <input className="inputField" list="surroundingList"
+                                        onChange={(event)=>this.handleInputField(event,"surrounding")}
+                                    />
+                                    <datalist id="surroundingList">
+                                      <option value="silent">silent</option>
+                                      <option value="quiet">quiet</option>
+                                      <option value="noisy">noisy</option>
+                                      <option value="loud">loud</option>
+                                      <option value="deafening">deafening</option>
+                                    </datalist>
+                              </div>
+                              <div id="mealInputGroup">
+                                  <label className="inputLabels">Meal &nbsp;</label>
+                                    <input className="inputField" list="mealList"
+                                          onChange={(event)=>this.handleInputField(event,"meal")}
+                                    />
+                                    <datalist id="mealList">
+                                      <option value="sweets">sweets</option>
+                                      <option value="fruit">fruit</option>
+                                      <option value="coffee">coffee</option>
+                                      <option value="energyDrink">energyDrink</option>
+                                      <option value="HeavyMeal">Roasted Chicken, Pork, Pizza</option>
+                                      <option value="LightMeal">Bread, Oat, Carrot</option>
+                                    </datalist>
+                              </div>
+                              <div id="sleepInputGroup">
+                                  <label className="inputLabels" >Sleep &nbsp;</label>
+                                    <input className="inputField" list="sleepAmount"
+                                          onChange={(event)=>this.handleInputField(event,"sleep")}
+                                    />
+                              </div>
+                              <div id="musicInputGroup">
+                                  <label className="inputLabels">Music &nbsp;</label>
+                                    <input className="inputField" list="musicList"
+                                          onChange={(event)=>this.handleInputField(event,"music")}
+                                    />
+                                    <datalist id="musicList">
+                                      <option value="focus">focus</option>
+                                      <option value="podcast">podcast</option>
+                                      <option value="lofi">lofi</option>
+                                      <option value="hiphop">energyDrink</option>
+                                      <option value="classic">classic</option>
+                                      <option value="jazz">jazz</option>
+                                    </datalist>
+                              </div>
+                              <div id="specialInputGroup">
+                                  <label className="inputLabels">Special &nbsp;</label>
+                                    <input className="inputField" list="specialList"
+                                          onChange={(event)=>this.handleInputField(event,"special")}
+                                    />
+                                    <datalist id="specialList">
+                                      <option value="sport">sport</option>
+                                      <option value="nap">nap</option>
+                                      <option value="walking">walking</option>
+                                      <option value="mb">mb</option>
+                                      <option value="game">game</option>
+                                    </datalist>
+                              </div>
+                              <input className="inputField" style={{width:"100%"}} onChange={this.handleChangeTextarea}
+                                   list="notes" id="note" name="note" value={this.state.note}
                             />
                                 <datalist id="notes">
                                     {inputOptions.map(
