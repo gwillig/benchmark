405d25ea6c445ab31109c50cb41f0aa41dc413bc
gwillig <gustavwillig@gmail.com>
2021-08-11 05:15:10 +0000

    FIX

--- home/views.py
+++ home/views.py
@@ -45,6 +45,8 @@ def data_stats(request):
     query = f"SELECT * FROM home_data WHERE date BETWEEN '{parameters['start_date']}' AND '{parameters['end_date']}'ORDER BY date"
     df_raw = pd.read_sql_query(query, connection)
     df = df_raw.sort_values(by=['date'], ascending=False)
+    '#3.Step: Round to hours'
+    df["date_round_hours"] = df["date"].dt.round('H')
     '#.Step: Change float to integer for histogram'
     df = df.astype({'result': 'int'})
     result = {}
@@ -55,20 +57,27 @@ def data_stats(request):
                              "histo_data": selectedRows["result"].value_counts().to_dict(),
                              "date": selectedRows["date"].iloc[0].strftime("%m/%d/%Y, %H:%M:%S")}
     else:
-        for attribute in df["note"].unique():
+        i=0
+        for attribute in df["date_round_hours"].unique():
+            print(i)
 
             if attribute not in [""]:
                 '#.Step: Select rows'
-                selectedRows = df.loc[df['note'] == attribute]
+                selectedRows = df.loc[df['date_round_hours'] == attribute]
+                if selectedRows["note"].iloc[0] == "earlyMorning; silent; LightMeal; 7:30; none; None":
+                    print("hello")
                 '#.Step: Prepare data for histogram'
                 histo_data = selectedRows["result"].value_counts().to_dict()
                 for el in range(0,12):
                     if histo_data.get(el) == None:
                         histo_data[el] = 0
-                result[attribute] = {"stats": selectedRows[["result"]].describe().fillna(0).round(1).to_dict(),
+                date = selectedRows["date"].iloc[0].strftime("%m/%d/%Y, %H:%M:%S")
+                note = selectedRows["note"].iloc[0]+"___"+ date
+                result[note] = {"stats": selectedRows[["result"]].describe().fillna(0).round(1).to_dict(),
                                      "count": selectedRows["result"].value_counts().to_dict(),
                                      "histo_data":histo_data,
-                                     "date": selectedRows["date"].iloc[0].strftime("%m/%d/%Y, %H:%M:%S")}
+                                     "date": date}
+                i = i +1
     return JsonResponse({"response": result})
 
 
